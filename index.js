346152705681711542cb29bf590576c9c89cf8b0
Dimitris <57366051+DimitrisBoutakoglou@users.noreply.github.com>
2025-04-07 11:47:23 +0300

    Added background image and basic text. Foundation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//
+//ftiakse rigged kartes card1,card2
+//prosthese tes kai deikse to sum
+//if statements gia to an einai 21, ligotero h perissotero
+//An einai 21, log blackjack, an einai ligotero grapse na parei ki allh karta ki an einai perissotero pes xasame
+
+
